7fdba7d01b2e643e7cf9bb4fad75f4dd059bfce4
Yazan Alqadery <yazanalqadery@gmail.com>
2025-04-26 19:45:23 +0300

    Expose endpoint for retrieving a single employee

--- src/main/java/com/yzn/springboot/crudapi/rest/EmployeeRestController.java
+++ src/main/java/com/yzn/springboot/crudapi/rest/EmployeeRestController.java
@@ -3,6 +3,7 @@ package com.yzn.springboot.crudapi.rest;
 import com.yzn.springboot.crudapi.entity.Employee;
 import com.yzn.springboot.crudapi.service.EmployeeService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +24,17 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> findAll(){
         return employeeService.findAll();
+    }
+
+    // Expose GET "/employees/{employeeId}" endpoint for retrieving a single employee
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId){
+
+        Employee employee = employeeService.findById(employeeId);
+
+        if(employee == null){
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+        return employee;
     }
 }
--- src/main/java/com/yzn/springboot/crudapi/service/EmployeeServiceImpl.java
+++ src/main/java/com/yzn/springboot/crudapi/service/EmployeeServiceImpl.java
@@ -3,6 +3,7 @@ package com.yzn.springboot.crudapi.service;
 import com.yzn.springboot.crudapi.DAO.EmployeeDAO;
 import com.yzn.springboot.crudapi.entity.Employee;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -18,5 +19,22 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public List<Employee> findAll() {
         return employeeDAO.findAll();
+    }
+
+    @Override
+    public Employee findById(int id) {
+        return employeeDAO.findById(id);
+    }
+
+    @Override
+    @Transactional
+    public Employee save(Employee employee) {
+        return employeeDAO.save(employee);
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(int id) {
+        employeeDAO.deleteById(id);
     }
 }
