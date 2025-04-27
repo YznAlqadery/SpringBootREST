3393da3d47c0a56d77236f36307e2864f179281a
Yazan Alqadery <yazanalqadery@gmail.com>
2025-04-27 21:01:00 +0300

    Add default page size to 20

--- src/main/java/com/yzn/springboot/crudapi/DAO/EmployeeRepository.java
+++ src/main/java/com/yzn/springboot/crudapi/DAO/EmployeeRepository.java
@@ -2,8 +2,11 @@ package com.yzn.springboot.crudapi.DAO;
 
 import com.yzn.springboot.crudapi.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 // JpaRepository<EntityType, Primary Key>
+// http://localhost:8080/api/members
+//@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
 
     // No need to write any code here
