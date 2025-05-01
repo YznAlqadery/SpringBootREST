e9f40205d60cac3f6da6b6a4006726c3f223702b
Yazan Alqadery <yazanalqadery@gmail.com>
2025-05-01 23:45:28 +0300

    Configured Spring Security to use custom database tables

--- src/main/java/com/yzn/springboot/crudapi/security/DemoSecurityConfig.java
+++ src/main/java/com/yzn/springboot/crudapi/security/DemoSecurityConfig.java
@@ -5,37 +5,57 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
 
+// Hard Coded Users
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsManager(){
+//
+//        UserDetails yazan = User.builder()
+//                .username("yazan")
+//                .password("{noop}test123")
+//                .roles("EMPLOYEE")
+//                .build();
+//
+//        UserDetails aws = User.builder()
+//                .username("aws")
+//                .password("{noop}test123")
+//                .roles("EMPLOYEE","MANAGER")
+//                .build();
+//
+//        UserDetails rashed = User.builder()
+//                .username("rashed")
+//                .password("{noop}test123")
+//                .roles("EMPLOYEE","MANAGER","ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(yazan,aws,rashed);
+//    }
+
+    // Add support for JDBC (no more hard coded users)
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager(){
+    // Inject data source that is Auto-configured by Spring Boot
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
 
-        UserDetails yazan = User.builder()
-                .username("yazan")
-                .password("{noop}test123")
-                .roles("EMPLOYEE")
-                .build();
+        // Tells Spring Security to use JDBC authentication with our data source
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
 
-        UserDetails aws = User.builder()
-                .username("aws")
-                .password("{noop}test123")
-                .roles("EMPLOYEE","MANAGER")
-                .build();
+        // Define query to retrieve a user by username
+        jdbcUserDetailsManager.setUsersByUsernameQuery("SELECT user_id,pw, active FROM members WHERE user_id=?");
 
-        UserDetails rashed = User.builder()
-                .username("rashed")
-                .password("{noop}test123")
-                .roles("EMPLOYEE","MANAGER","ADMIN")
-                .build();
+        // Define query to retrieve the authorities/roles by username
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("SELECT user_id, role FROM roles WHERE user_id=?");
 
-        return new InMemoryUserDetailsManager(yazan,aws,rashed);
+        return jdbcUserDetailsManager;
     }
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
